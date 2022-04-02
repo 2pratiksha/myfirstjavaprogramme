624bd4be01e75c1c9ceca4a00aef72995d152a38
pratikshadesai <dk_amz@yahoo.com>
2022-04-02 12:01:24 +0100

    this is my first java programme

---
+++ ManualTestingTopics.java
@@ -0,0 +1,30 @@
+package com.learning.java;
+
+public class ManualTestingTopics {
+    public static void main(String[] args) {
+        System.out.println("1.What is Software?");
+        System.out.println("2.What is Software Testing?");
+        System.out.println("3.Why Software Testing is Important?");
+        System.out.println("4.What is SDLC?");
+        System.out.println("5.What is STLC?");
+        System.out.println("6.Qualification of Tester.");
+        System.out.println("7.Challenges of Tester.");
+        System.out.println("8.Challenges with Manual Testing.");
+        System.out.println("9.Principle of Software Testing.");
+        System.out.println("10.Explain about Test Plan.");
+        System.out.println("11.Why do we write Test Scenario?");
+        System.out.println("12.What is the meaning of RTM?");
+        System.out.println("13.What is difference between Test Scenario and Test Case?");
+        System.out.println("14.Software Testing Techniques.");
+        System.out.println("15.What is Bug?");
+        System.out.println("16.Why are bug in Software?");
+        System.out.println("17.Explain Bug Cycle.");
+        System.out.println("18.What is Smoke and Sanity Testing?");
+        System.out.println("19.What is Severity and Priority?");
+        System.out.println("20.What is Blackbox Testing and Whitebox Testing?");
+        System.out.println("21.Explain about static and Dynamic Testing also Exploratory and Ad-hoc Testing.");
+        System.out.println("22.Explain about Waterfall Model and V-Model and Agile Model.");
+        System.out.println("23.What is TDD and BDD?");
+
+    }
+}
